a2c25a325f50c1f6df5f324b6f24442999793544
wallberg <wallberg@983cd692-402e-0410-a370-846aaee464cf>
2012-06-19 12:45:45 +0000

    initial code: Usage, UserAgent
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/webreports@3113 983cd692-402e-0410-a370-846aaee464cf

---
+++ src/edu/umd/lib/webreports/Usage.groovy
@@ -0,0 +1,280 @@
+package edu.umd.lib.webreports
+
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.PosixParser
+
+@Grab('log4j:log4j:1.2.16')
+import org.apache.log4j.Logger
+import org.apache.log4j.PatternLayout
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.Priority
+
+class Usage {
+
+  static Logger log = Logger.getInstance(Usage.class)
+
+  // statistics
+  static Map stat = [
+    'files':0,
+    'lines':0,
+    'errors':0,
+  ]
+
+  static List logFileNames
+
+  public static void main(args) {
+    try {
+
+      // Get command line options
+      parseCommandLine(args)
+
+      // Get system properties
+      Properties p = System.properties
+
+      // Initialize UserAgent
+      UserAgent.readConfig()
+      
+      // Process each log file
+      logFileNames.each { fileName ->
+        File f = new File(fileName)
+
+        if (! f.canRead()) {
+          log.warn("Unable to read ${f}")
+        } else {
+          stat.files++
+
+          InputStream is = new FileInputStream(f)
+          if (f.name.endsWith(".gz")) {
+            is = new GZIPInputStream(is)
+          }
+          BufferedReader r = new BufferedReader(new InputStreamReader(is, "UTF-8"))
+
+          r.eachLine { line ->
+            stat.lines++
+
+            Matcher m = (line =~ /^([^ ]+?) ([^ ]+?) ([^ ]+?) \[(.*?)\] "(?:[A-Z]+? )?(.+?)(?: .+?)?(?<!\\)" (\d{3}) ([^ ]+) "(.*?)(?<!\\)" "(.*?)(?<!\\)"$/)
+
+            if (! m.matches()) {
+              log.error("line doesn't match: ${line}")
+              count.errors++
+            } else {
+
+              def (all,host,foo0,foo1,date,url,code,bytes,referer,ua) = m[0]
+
+              // ua fixup
+              ua = ua.toLowerCase()
+
+              // url fixup
+              url = url
+                  .replaceAll(/\?.*$/,'')
+//                  .toLowerCase()
+
+//              // filter
+//              if (! reject(url, code, ua)) {
+//                dst.write(l)
+//                dst.write("\n")
+//                count.write++
+//              } else if (rej) {
+//                rej.write(l)
+//                rej.write("\n")
+//              }
+            }
+          }
+
+          r.close()
+
+        }
+      }
+
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1)
+    }
+    finally {
+      println """
+Statistics:
+  log files
+    files read:   ${stat.files} 
+    lines read:   ${stat.lines}
+    parse errors: ${stat.errors}
+"""
+    }
+
+    System.exit(0)
+  }
+
+  /**********************************************************************/
+  /*
+   * Get a local log files.
+   */
+
+  def getFilesLocal() {
+
+    srcdir = new File(srcuri)
+
+    if (! (dstdir.exists() && dstdir.isDirectory())) {
+      log.error("destination directory does not exist: ${dstdir}")
+      System.exit(1)
+    }
+
+    log.info("Source directory: ${srcdir}")
+
+    // Iterate over matching source files
+    first = true
+    srcdir.traverse(nameFilter:srcpat, maxDepth:0, sort:{a,b->a.name<=>b.name}) { srcfile ->
+      count = ['read':0, 'write':0, 'error':0]
+
+      if (first) {
+        UserAgent.readConfig()
+        first = false
+      }
+
+      log.info("Processing ${srcfile.name}")
+
+      dstfile = new File(dstdir, srcfile.name)
+      log.info("...copying,filtering to ${dstfile.name}")
+
+      // Read and write gziped file
+      src = new InputStreamReader(new GZIPInputStream(new FileInputStream(srcfile)),"UTF-8")
+      dst = new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(dstfile)),"UTF-8")
+      rej = null
+      if (rejdir) {
+        rejfile = new File(rejdir, srcfile.name)
+        rej = new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(rejfile)),"UTF-8")
+      }
+
+      // Process each line
+      src.eachLine { l ->
+        count.read++
+
+        // parse the line
+        m = (l =~ /^([^ ]+?) ([^ ]+?) ([^ ]+?) \[(.*?)\] "(?:[A-Z]+? )?(.+?)(?: .+?)?(?<!\\)" (\d{3}) ([^ ]+) "(.*?)(?<!\\)" "(.*?)(?<!\\)"$/)
+
+        if (! m.matches()) {
+          log.error("line doesn't match: ${l}")
+          count.error++
+        } else {
+
+          def (all,host,foo0,foo1,date,url,code,bytes,referer,ua) = m[0]
+
+          // ua fixup
+          ua = ua.toLowerCase()
+
+          // url fixup
+          url = url
+              .replaceAll(/\?.*$/,'')
+              .toLowerCase()
+
+          // filter
+          if (! reject(url, code, ua)) {
+            dst.write(l)
+            dst.write("\n")
+            count.write++
+          } else if (rej) {
+            rej.write(l)
+            rej.write("\n")
+          }
+        }
+      }
+
+      dst.close()
+      src.close()
+      if (rej) {
+        rej.close()
+      }
+
+      // Delete the src file
+      if (! preserve) {
+        srcfile.delete()
+        log.info("...deleted ${srcfile.name}")
+      }
+
+      log.info("...line counts: ${count}")
+    }
+  }
+
+
+  /**********************************************************************/
+  /**
+   * Reject a line base on url, return code, or user agent.
+   */
+  def reject(url, code, ua) {
+
+    reUrl = [
+      /^\/(images|styles|robots.txt|archivesum|blogs|cgi-bin|dcr|digital|drum|etc\/local\/(emw|coldwar))/,
+      /\.(jpg|gif|ico|png|bmp|js|css)$/,
+    ]
+
+    // url filter
+    for (pat in reUrl) {
+      if ((url =~ pat).find()) {
+        return true
+      }
+    }
+
+    // return code filter
+    if (! (code =~ /[23]\d\d/)) {
+      return true
+    }
+
+    // user agent filter
+    return ! UserAgent.isBrowser(ua)
+  }
+
+  /*
+   * Parse the command line.
+   */
+
+  static void parseCommandLine(args) {
+    // Setup the options
+    Options options = new Options()
+
+    //    Option option = new Option("s", "site", true, "site to transfer logs from")
+    //    option.setRequired(true)
+    //    options.addOption(option)
+
+    // Check for help
+    if ("-h" in args) {
+      printUsage(options)
+    }
+
+    // Parse the command line
+    PosixParser parser = new PosixParser()
+    CommandLine cmd
+    try {
+      cmd = parser.parse(options, args)
+    }
+    catch (Exception e) {
+      println e.class.name + ': ' + e.message
+      println ''
+      printUsage(options)
+    }
+
+    // Validate results
+    logFileNames = cmd.getArgList()
+  }
+
+  /*
+   * Print program usage.
+   */
+
+  static void printUsage(Options options, Object[] args) {
+    // print messages
+    args.each {println it}
+    if (args.size() != 0) { println '' }
+
+    HelpFormatter formatter = new HelpFormatter()
+    formatter.printHelp("getLogs [-h] <apache log>...\n", options)
+
+    System.exit(1)
+  }
+}
---
+++ src/edu/umd/lib/webreports/UserAgent.groovy
@@ -0,0 +1,200 @@
+package edu.umd.lib.webreports
+
+import java.security.*
+
+@Grab('dom4j:dom4j:1.4')
+import org.dom4j.*
+import org.dom4j.io.*
+
+@Grab('commons-lang:commons-lang:2.5')
+import org.apache.commons.lang.StringUtils
+
+@Grab('commons-httpclient:commons-httpclient:3.1')
+import org.apache.commons.httpclient.*
+import org.apache.commons.httpclient.methods.*
+import org.apache.commons.httpclient.methods.multipart.*
+import org.apache.commons.httpclient.params.*
+
+@Grab('log4j:log4j:1.2.16')
+import org.apache.log4j.Logger
+
+
+public class UserAgent {
+  String id
+  String string
+  String desc
+  List   types
+  String comment
+  
+  private static auths = []
+  private static db = new File('data/allagents.xml')
+  private static dbNew = new File('data/allagents-new.xml')
+  
+  private static log = Logger.getInstance(UserAgent.class)
+
+
+  /**
+   * Refresh the user-agent database, if it has changed.
+   */
+
+  public static downloadDb() {
+
+    log.info("Downloading User-Agent database")
+
+    try {
+      log.info("...downloading file")
+
+      // download new file and calculate it's digest
+      def client = new HttpClient()
+      def method = new GetMethod('http://www.user-agents.org/allagents.xml')
+
+      def status = client.executeMethod(method)
+
+      assert status == HttpStatus.SC_OK
+
+      def digestNew = new DigestInputStream(method.responseStream, MessageDigest.getInstance('MD5'))
+      def fNew = new FileOutputStream(dbNew)
+
+      fNew.leftShift(digestNew)
+
+      digestNew.close()
+      fNew.close()
+
+      if (db.exists()) {
+        // calculate current file's digest
+        def digest = new DigestInputStream(new FileInputStream(db), MessageDigest.getInstance('MD5'))
+        def b = new byte[1024]
+        while (digest.read(b,0,1024) != -1) {}
+        digest.close()
+        
+        if (digest.messageDigest.digest() == digestNew.messageDigest.digest()) {
+          log.info("...file has not changed")
+          dbNew.delete()
+          return
+        }
+      }
+
+      // ensure the new file is readable, parseable
+      def reader = new SAXReader()
+      def doc = reader.read(dbNew)
+    }
+    catch (Throwable t) {
+      log.error("...error downloading new file:\n${t.message}")
+      return
+    }
+
+    // new file has been successfully downloaded and parsed
+    log.info("...installing new file")
+
+    if (db.exists()) {
+      db.delete()
+    }
+    dbNew.renameTo(db)
+  }
+
+
+  /**
+   * Read authoritative user agent list 
+   */
+
+  public static readConfig() {
+    downloadDb()
+
+    def reader = new SAXReader()
+    def doc = reader.read(db)
+    doc.selectNodes("/user-agents/user-agent").each { node -> 
+      auths.add(newInstance(node))
+    }
+  }
+
+
+  /**
+   * Build new instance of UserAgent from <user-agent> node
+   */
+
+  public static UserAgent newInstance(Node n) {
+    def ua = new UserAgent()
+
+    ua.id = n.selectSingleNode('ID').text
+    ua.string = n.selectSingleNode('String').text
+    ua.desc = n.selectSingleNode('Description').text
+    ua.comment = n.selectSingleNode('Comment').text
+
+    ua.types = n.selectSingleNode('Type').text.split(/ /) as List
+
+    return ua
+  }
+
+
+  /**
+   * Compute the match between user agent strings
+   */
+
+  public Float getMatch(String ua) {
+    def uan = uaNormalize(ua)
+    def authn = uaNormalize(this.string)
+
+    def d = StringUtils.getLevenshteinDistance(uan,authn)
+
+    return (float)d / (float)uan.length()
+  }
+
+
+  /**
+   * Get the best match against the entire database.
+   *
+   * @return List; [0] = match, [1] = UserAgent
+   */
+
+  private static Map bestMatchCache = [:]
+  public static List getBestMatch(String ua) {
+    if (! bestMatchCache.containsKey(ua)) {
+      def ret = [null,null]  // [match, UserAgent]
+
+      // Iterate over auths
+      for (auth in auths) {
+        Float f = auth.getMatch(ua)
+
+        if (f == 0.0 || ret[0] == null || f < ret[0]) {
+          ret[0] = f
+          ret[1] = auth
+        }
+
+        if (f == 0.0) {
+          break
+        }
+      }
+
+      bestMatchCache[ua] = ret
+    }
+
+    return bestMatchCache[ua]
+  }
+
+
+  /**
+   * Determine if the user agent is a browser.
+   */
+
+  public static boolean isBrowser(String ua) {
+    def m = getBestMatch(ua)
+    return 'B' in m[1].types
+  }
+
+  /**
+   * Normalize a user-agent string.
+   */
+
+  private static Map uaNormCache = [:]
+  private static String uaNormalize(s) {
+    if (! uaNormCache.containsKey(s)) {
+      uaNormCache[s] = s
+      .toLowerCase()
+      .replaceAll(/; feed-id=\d+/,'')
+      .replaceAll(/^.*compatible;+/,'')
+    }
+
+    return uaNormCache[s]
+  }
+
+}
