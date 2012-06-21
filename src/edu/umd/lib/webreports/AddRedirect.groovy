ebf4084c3db4dd5e208797419982eb1dd25f71a1
wallberg <wallberg@983cd692-402e-0410-a370-846aaee464cf>
2012-06-21 15:11:31 +0000

    add shell of new AddRedirect
    git-svn-id: https://svn.lib.umd.edu/webs/trunk/webreports@3137 983cd692-402e-0410-a370-846aaee464cf

---
+++ src/edu/umd/lib/webreports/AddRedirect.groovy
@@ -0,0 +1,173 @@
+package edu.umd.lib.webreports
+
+import groovy.io.FileVisitResult;
+
+import java.net.URLDecoder
+
+import java.util.zip.GZIPInputStream
+import java.util.zip.GZIPOutputStream
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import java.util.TreeMap
+
+import java.text.SimpleDateFormat
+
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.PosixParser
+
+@Grab('log4j:log4j:1.2.16')
+import org.apache.log4j.Logger
+import org.apache.log4j.PatternLayout
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.Priority
+
+/**
+ * Generate usage for report for /www files combined with urls from apache logs.
+ * 
+ * Report CSV file fields:
+ * <pre>
+ *   File
+ *   Last Modified
+ *   URL*
+ *   result code
+ *   hits
+ *   hits/browser
+ *   hits/bot
+ * </pre>
+ * 
+ * @author wallberg
+ *
+ */
+class AddRedirect {
+
+  static Logger log = Logger.getInstance(AddRedirect.class)
+
+  // statistics
+  static Map stat = [:].withDefault { v -> 0l }
+
+  static File infile = null
+  static File outfile = null
+
+  public static void main(args) {
+    try {
+
+      // Get command line options
+      parseCommandLine(args)
+
+      // Get system properties
+      Properties p = System.properties
+
+      BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(infile), "UTF-8"))
+      PrintWriter w = new PrintWriter(new FileOutputStream(outfile))
+
+      boolean first = true
+
+      r.eachLine { line ->
+        stat.read++
+
+        List row = line.split(/,(?=([^\"]*\"[^\"]*\")*[^\"]*$)/,-1)
+
+        String redirectsTo = ''
+        if (first) {
+          redirectsTo = 'Redirects to'
+          first = false
+        } else {
+          List results = row[4].substring(1,row[4].length()-1).split(/,/)
+
+          if (results.any { it =~ /3\d\d/ }) {
+            stat.redirect++
+            redirectsTo = 'redirect'
+          }
+        }
+
+        row << '"' + redirectsTo.replaceAll("\"","\"\"") + '"'
+
+        w.println(row.join(','))
+      }
+
+      r.close()
+      w.close()
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      System.exit(1)
+    }
+    finally {
+      println """
+Statistics:
+
+  rows read: ${stat.read}
+
+  redirects found: ${stat.redirect}
+  redirects added: ${stat.add}
+"""
+    }
+
+    System.exit(0)
+  }
+
+  /*
+   * Parse the command line.
+   */
+
+  static void parseCommandLine(args) {
+    // Setup the options
+    Options options = new Options()
+
+    Option option = new Option("i", "input", true, "input CSV file")
+    option.setRequired(true)
+    options.addOption(option)
+
+    option = new Option("o", "output", true, "output CSV file")
+    option.setRequired(true)
+    options.addOption(option)
+
+    // Check for help
+    if ("-h" in args) {
+      printUsage(options)
+    }
+
+    // Parse the command line
+    PosixParser parser = new PosixParser()
+    CommandLine cmd
+    try {
+      cmd = parser.parse(options, args)
+    }
+    catch (Exception e) {
+      println e.class.name + ': ' + e.message
+      println ''
+      printUsage(options)
+    }
+
+    // Validate results
+    if (cmd.getArgList().size() > 0) {
+      printUsage(options)
+    }
+
+    infile = new File(cmd.getOptionValue('i'))
+    if (!infile.canRead()) {
+      printUsage(options, "Unable to open '$infile' for reading");
+    }
+
+    outfile = new File(cmd.getOptionValue('o'))
+  }
+
+  /*
+   * Print program usage and exit.
+   */
+
+  static void printUsage(Options options, Object[] args) {
+    // print messages
+    args.each {println it}
+    if (args.size() != 0) { println '' }
+
+    HelpFormatter formatter = new HelpFormatter()
+    formatter.printHelp("AddRedirect [-h] -i <input> -o <output>\n", options)
+
+    System.exit(1)
+  }
+
+}
